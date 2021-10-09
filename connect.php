60f1e9c92db61d63df246a64c0b12a533608cba4
vaidehii-u <87386351+vaidehii-u@users.noreply.github.com>
2021-10-09 22:36:07 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,21 +1,21 @@
 <?php
-$username = filter_input(INPUT_POST, 'username');
+$email = filter_input(INPUT_POST, 'email');
 $password = filter_input(INPUT_POST, 'password');
-if (!empty($username)){
+if (!empty($email)){
 if (!empty($password)){
 $host = "localhost";
-$dbusername = "root";
+$dbemail = "root";
 $dbpassword = "";
 $dbname = "youtube";
-// Create connection
-$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+// Create connections
+$conn = new mysqli ($host, $dbemail, $dbpassword, $dbname);
 if (mysqli_connect_error()){
 die('Connect Error ('. mysqli_connect_errno() .') '
 . mysqli_connect_error());
 }
 else{
-$sql = "INSERT INTO account (username, password)
-values ('$username','$password')";
+$sql = "INSERT INTO account (email, password)
+values ('$email','$password')";
 if ($conn->query($sql)){
 echo "New record is inserted sucessfully";
 }
@@ -32,7 +32,7 @@ die();
 }
 }
 else{
-echo "Username should not be empty";
+echo "Email/phone number should not be empty";
 die();
 }
 ?>
